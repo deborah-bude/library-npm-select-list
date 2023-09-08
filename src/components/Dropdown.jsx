86c81b025a98fe92776727718f1a15579f24092d
Déborah <deborah.bude@gmail.com>
2023-09-08 16:16:24 +0200

    Remove group to dropdown menu and style components

--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -1,75 +1,63 @@
 import '../App.css'
-//import {OptionDropdown} from "./Option.jsx";
 import PropTypes from 'prop-types';
-import {useState} from "react";
+import {useEffect, useRef, useState} from "react";
 
-export function Dropdown(props) {
+export function Dropdown(props, { search = false}) {
     const dropdownArray = [...props.options]
     const labelId = props.title.toLowerCase().replaceAll(" ", "-")
     const [openDropdown, setOpenDropdown] = useState(false)
     const [chosenValue, setChosenValue] = useState("")
     const [optionSelected, setOptionSelected] = useState([...dropdownArray])
+    const ref = useRef(null);
 
-    if(props.search === undefined) props.search = false
+    useEffect(() => {
+        const handleOutSideClick = (event) => {
+            if (!ref.current?.contains(event.target)) {
+                closeDropdown()
+            }
+        };
+        window.addEventListener("mousedown", handleOutSideClick);
+    }, [ref]);
 
-    function dropdownClick(e) {
-        setOpenDropdown(!openDropdown)
-    }
     function closeDropdown() {
         setOpenDropdown(false)
     }
     function selectOption(valueOption) {
-        console.log(valueOption)
         setChosenValue(valueOption)
     }
 
     function searchValue(e) {
-        console.log(optionSelected)
         if(e.target.value.length === 0) {
             setOptionSelected(dropdownArray)
         }
         else {
             setOptionSelected(dropdownArray.filter((elementArray) =>
-                elementArray.type === "group" ?
-                    elementArray.items.filter((groupItem) => groupItem.toLowerCase().includes(e.target.value.toLowerCase()))
-                    :
                     elementArray.toLowerCase().includes(e.target.value.toLowerCase())
             ))
-            console.log(optionSelected)
         }
         setChosenValue(e.target.value)
     }
 
     function OptionElement(option) {
-        if (option.type === "group") {
-            return (
-                <ul key={option.name}>{option.name}
-                    {option.items.map((item) => {
-                            return <li key={item} onClick={() => {selectOption(item)}}>{item}</li>
-                        }
-                    )}
-                </ul>
-            )
-        }
         return <li key={option} onClick={() => {selectOption(option)}}>{option}</li>
     }
 
     return (
-        <div className="dropdown" onClick={dropdownClick}>
+        <div className="dropdown" onClick={() => setOpenDropdown(!openDropdown)}>
             {props.search ?
                 <>
-                    <label htmlFor={labelId}>{props.title}</label>
+                    <label className="label-dropdown" htmlFor={labelId}>{props.title}</label>
                     <input name={labelId} id={labelId} className="chosen-value" type="text" value={chosenValue}
                            placeholder={openDropdown ? "Select state" : "Type to filter"} onChange={searchValue}/>
                 </>
             :
                 <>
-                    <p>{props.title}</p>
-                    <p>{chosenValue === "" ? "Select state" : chosenValue }</p>
+                    <p className="label-dropdown">{props.title}</p>
+                    <p className="input-dropdown">{chosenValue === "" ? "Select state" : chosenValue }</p>
                     <input name={labelId} id={labelId} type="hidden" value={chosenValue} />
                 </>
             }
-            <div className={openDropdown ? "open dropdown-menu" : "dropdown-menu"}>
+            <div ref={ref} className={openDropdown ? "open dropdown-menu" : "dropdown-menu"}>
                 <ul className="value-list">
                     { optionSelected.map((option) => OptionElement(option)) }
                 </ul>
@@ -81,15 +69,7 @@ export function Dropdown(props) {
 Dropdown.propTypes = {
     title: PropTypes.string.isRequired,
     search: PropTypes.bool,
-    //onSelect: PropTypes.func.isRequired,
     options: PropTypes.arrayOf(
-        PropTypes.oneOfType([
             PropTypes.string.isRequired,
-            PropTypes.shape({
-                type: 'group',
-                name: PropTypes.string.isRequired,
-                items: PropTypes.arrayOf(PropTypes.string.isRequired)
-            }),
-        ])
     ).isRequired
 }
--- src/main.jsx
+++ src/main.jsx
@@ -19,18 +19,19 @@ import './App.css'
     },
 ]*/
 
-const optionSelect = ["alabama", "Alaska", "Arizona"]
+const optionSelect = [
+    "Alabama", "Alaska", "Arizona", "Arkansas", "California", "Colorado", "Connecticut",
+    "Delaware", "Florida", "Georgia", "Hawaii", "Idaho", "Illinois", "Indiana", "Iowa", "Kansas",
+    "Kentucky", "Louisiana", "Maine", "Maryland", "Massachusetts", "Michigan", "Minnesota",
+    "Mississippi", "Missouri", "Montana", "Nebraska", "Nevada", "New Hampshire", "New Jersey",
+    "New Mexico", "New York", "North Carolina", "North Dakota", "Ohio", "Oklahoma", "Oregon",
+    "Pennsylvania", "Rhode Island", "South Carolina", "South Dakota", "Tennessee", "Texas", "Utah",
+    "Vermont", "Virginia", "Washington", "West Virginia", "Wisconsin", "Wyoming"]
 
-const optionsGroup = [
-    { type: 'group', name: 'Foods', items: ["Chocolate", "Vanilla", "Strawberry"]},
-    { type: 'group', name: 'Colors', items: ["Blue", "White", "Red"]}
-]
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-      <Dropdown title="Title select 1" options={optionSelect} search={false} />
-      <Dropdown title="Title select 1" options={optionsGroup} search={false} />
+      <Dropdown title="Title select 1" options={optionSelect} />
       <Dropdown title="Title select 2" options={optionSelect} search={true} />
-      <Dropdown title="Title select 1" options={optionsGroup} search={true} />
   </React.StrictMode>,
 )
